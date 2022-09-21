f155052bbbacaa93ec002daa2806c786117f9fe9
Manuel Sosa <mr.sosa@uniandes.edu.co>
2022-09-21 14:06:05 -0500

    Config

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- app.enableVersioning({
-   type: VersioningType.URI,
-   prefix: 'api/v',
-   defaultVersion: '1',
- });
- await app.listen(process.env.PORT || 3010);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+    defaultVersion: '1',
+  });
+  await app.listen(process.env.PORT || 3010);
 }
 bootstrap();
